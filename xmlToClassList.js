64b28a72a7387617c67deadc957ce21822e5a2ea
max <max@maxhogan.dev>
2022-11-24 02:11:26 -0500

    Add custom error

--- xmlToClassList.js
+++ xmlToClassList.js
@@ -1,14 +1,29 @@
 import {XMLParser} from 'fast-xml-parser';
 import {readFileSync} from 'fs';
 import {load} from 'cheerio';
+import {strict as assert} from "node:assert";
 
 const parser = new XMLParser();
 
+class ICStateStaleError extends Error {
+    constructor (message) {
+        super(message)
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
 const xmlToClassList = xml => {
     const parsed = parser.parse(xml);
-
     const mainField = parsed.PAGE.FIELD.join("\n");
+    const parsedAgain = parser.parse(mainField);
 
+    const icStateStale = !parsedAgain.tr.td.DIV;
+    const icStateStale2 = parsedAgain.tr.td.table.tr[0].td === "This page is no longer available.";
+    assert.equal(icStateStale, icStateStale2, "icStateStale and icStateStale2 should be equal");
+
+    if (icStateStale) {
+        throw new ICStateStaleError(`Failed parse XML because ic state is stale`);
+    }
 
     const $ = load(mainField);
     const numSectionsText = $(".PSGROUPBOXLABEL").text();
