52886cccaa0236ab55f634dd6d2c342c8f78701a
max <max@maxhogan.dev>
2022-11-27 15:23:26 -0600

    Test nextMeetingOfOccurrence

--- tests/nextMeeting.test.js
+++ tests/nextMeeting.test.js
@@ -1,5 +1,5 @@
 import {describe, expect, test} from "@jest/globals";
-import {timeToComponents} from "../lib/findRooms/nextMeeting.js";
+import {nextMeetingOfOccurrence, timeToComponents} from "../lib/findRooms/nextMeeting.js";
 
 describe("nextMeeting", () => {
     test("timeToComponents", () => {
@@ -11,5 +11,14 @@ describe("nextMeeting", () => {
         expect(timeToComponents("12:00am")).toEqual([0, 0]);
         expect(timeToComponents("12:23pm")).toEqual([12, 23]);
         expect(timeToComponents("12:23am")).toEqual([0, 23]);
+    });
+
+    test("nextMeetingOfOccurrence", () => {
+        // in the future
+        expect(nextMeetingOfOccurrence(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T01:15:00"), new Date("2020-12-31T23:59:59"))).toEqual(new Date("2021-01-01T00:00:00"));
+        // in the past
+        expect(nextMeetingOfOccurrence(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T01:15:00"), new Date("2021-01-01T01:15:01"))).toEqual(null);
+        // ongoing
+        expect(nextMeetingOfOccurrence(new Date("2021-01-01T00:00:00"), new Date("2021-01-01T01:15:00"), new Date("2021-01-01T00:30:00"))).toEqual(new Date("2021-01-01T00:30:00"));
     });
 });
