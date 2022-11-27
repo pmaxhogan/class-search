a02b82f07e29aa3c395f7e4c0045ab22f9d5079a
max <max@maxhogan.dev>
2022-11-27 15:21:23 -0600

    Test timeToComponents

--- tests/nextMeeting.test.js
+++ tests/nextMeeting.test.js
@@ -1,7 +1,15 @@
 import {describe, expect, test} from "@jest/globals";
+import {timeToComponents} from "../lib/findRooms/nextMeeting.js";
 
 describe("nextMeeting", () => {
-    test("nextMeeting", () => {
-        expect(true).toBe(true);
+    test("timeToComponents", () => {
+        expect(timeToComponents("1:00pm")).toEqual([13, 0]);
+        expect(timeToComponents("1:00am")).toEqual([1, 0]);
+        expect(timeToComponents("2:23pm")).toEqual([14, 23]);
+        expect(timeToComponents("2:23am")).toEqual([2, 23]);
+        expect(timeToComponents("12:00pm")).toEqual([12, 0]);
+        expect(timeToComponents("12:00am")).toEqual([0, 0]);
+        expect(timeToComponents("12:23pm")).toEqual([12, 23]);
+        expect(timeToComponents("12:23am")).toEqual([0, 23]);
     });
 });
