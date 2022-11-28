097863f4dc1f6b9332ed75003c0890a21dac0f61
max <max@maxhogan.dev>
2022-11-28 01:55:00 -0600

    Connect frontend to backend

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,14 @@
+// noinspection JSUnusedGlobalSymbols
+
 import functions from "firebase-functions";
 import {stat} from "fs/promises";
 import {getSectionsFromDisk} from "./lib/coursebook/getSections.js";
+import e from "express";
 
 const sections = await getSectionsFromDisk();
 
-export const helloWorld = functions.https.onRequest(async (request, response) => {
-  functions.logger.info("Hello logs! " + await stat("export/allSections.json"), {structuredData: true});
-  functions.logger.info("Loaded " + sections.length, {structuredData: true});
-  response.send("" + sections.length);
-});
+const app = e();
+
+app.get("/api", (req, res) => res.send("Loaded " + sections.length + " sections"));
+
+export const api = functions.https.onRequest(app);
--- hosting/pages/index.tsx
+++ hosting/pages/index.tsx
@@ -4,6 +4,12 @@ export default function IndexPage() {
   return (
     <div>
       Hello World. <Link href="/about">About</Link>
+        <button onClick={click}>Click me</button>
     </div>
   )
+}
+
+async function click() {
+    const text = await fetch("/api/").then(res => res.text());
+    alert(text);
 }
